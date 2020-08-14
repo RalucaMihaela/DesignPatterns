6b1fa6a9945681671c7064e808931f35e5c49363
RalucaMihaela <ionescu.raluca@agilefreaks.com>
2020-08-14 16:44:33 +0300

    Code format

--- Intermediate Design Patterns/MVVM Pattern/CoffeeQuest/CoffeeQuest/Controllers/ViewController.swift
+++ Intermediate Design Patterns/MVVM Pattern/CoffeeQuest/CoffeeQuest/Controllers/ViewController.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2019 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import MapKit
 import YelpAPI
 
@@ -37,6 +9,7 @@ public class ViewController: UIViewController {
   private let locationManager = CLLocationManager()
   
   // MARK: - Outlets
+  
   @IBOutlet public var mapView: MKMapView! {
     didSet {
       mapView.showsUserLocation = true
@@ -44,6 +17,7 @@ public class ViewController: UIViewController {
   }
   
   // MARK: - View Lifecycle
+  
   public override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -74,9 +48,8 @@ extension ViewController: MKMapViewDelegate {
   
   private func searchForBusinesses() {
     let coordinate = mapView.userLocation.coordinate
-    guard coordinate.latitude != 0,
-          coordinate.longitude != 0 else {
-      return
+    guard coordinate.latitude != 0, coordinate.longitude != 0 else {
+        return
     }
     
     let yelpCoordinate = YLPCoordinate(latitude: coordinate.latitude,
@@ -88,8 +61,7 @@ extension ViewController: MKMapViewDelegate {
                   offset: 0,
                   sort: .bestMatched) { [weak self] (searchResult, error) in
       guard let self = self else { return }
-      guard let searchResult = searchResult,
-            error == nil else {
+      guard let searchResult = searchResult, error == nil else {
         print("Search failed: \(String(describing: error))")
         return
       }
@@ -132,8 +104,7 @@ extension ViewController: MKMapViewDelegate {
     }
   }
   
-  public func mapView(_ mapView: MKMapView,
-                      viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+  public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
     guard let viewModel = annotation as? BusinessMapViewModel else {
       return nil
     }
